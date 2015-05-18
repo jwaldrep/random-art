f7ffa3c54000ca1973dbd7ed1bae8cc997a0d5de
John Waldrep <johnwaldrep@gmail.com>
2015-05-18 15:19:38 -0400

    Basic functionality working

--- random_art.py
+++ random_art.py
@@ -1,35 +1,35 @@
 import random
-from math import cos, sin, tan
+from math import cos, sin, tan, log10
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
 # Your expression should have a __str__() function defined for it.
 
+def avg(x, y): return (x + y) / 2;
 
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
 
-    rtrix = lambda x,y: random.triangular(0,x,y)
-    rtriy = lambda x,y: random.triangular(0,y,x)
-    rgau = lambda x,y: random.gauss(x,y)
-    cos = lambda x:  math.cos(x)
-    sin = lambda x: math.sin(x)
 
-    def tritry():
-        expr1 = lambda x, y: (x + y)/2
-        expr2 = lambda x, y: (x - y)/2
-        expr3 = lambda x, y: x * y
-        return random.choice([expr1, expr2, expr3])
-    exprs_1 = ['cos(z)', 'sin(z)', 'tan(z)']
-    exprs_2 = ['random.triangular(x,y)',
-               'random.gauss(x,y)',
+    exprs_1 = ['cos(z)', 'sin(z)', 'tan(z)',
+               'z**2',
                ]
+    exprs_2 = [#'random.triangular(x,y)',
+               #'random.gauss(x,y)',
+               'avg(x,y)',
+               '(x*y)',
+               'x', 'y',
+            #    'random.expovariate(x/(y+1))',
+               ]
+
     big_expr = random.choice(exprs_1)
 
-    for expr in exprs_1:
-        big_expr = big_expr.replace('z', expr)
+    for _ in range(random.randrange(30)):
+        big_expr = big_expr.replace('z', random.choice(exprs_1))
+
     big_expr = big_expr.replace('z', random.choice(exprs_2))
+
     return big_expr
 
 def run_expression(expr, x, y):
